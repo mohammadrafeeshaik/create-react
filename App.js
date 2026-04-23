a1471242b9e37ff2c6397133c49ee2728b336610
Rafee Shaik <m.rafee.sk@gmail.com>
2026-04-23 17:48:13 +0530

    added npm scripts

--- App.js
+++ App.js
@@ -1,25 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
-  ]),
-]);
-
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React",
-);
-console.log(heading);
+const heading = <h1 id="heading">Learning React JSX</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(root);
 
-root.render(parent);
+root.render(heading);
--- README.md
+++ README.md
@@ -32,3 +32,22 @@
     - HTTPs
     - Tree Shaking (removes unused code)
     - Different dev and build bundles
+
+# browserslist (browserslist.dev) support for browsers
+
+"browserslist": [
+"last 2 versions"
+]
+
+# npm scripts
+
+"scripts": {
+"start": "parcel index.html",
+"build": "parcel build index.html"
+}
+
+# JSX (Transplied before it reaches JS engine)
+
+React.createElement => React element-JS object => HTML element(render)
+
+JSX => React.createElement => React element-JS object => HTML element(render)
